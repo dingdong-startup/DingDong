eb8c5b858940973695bc57554ba7062a42046837
Segrelove <mlesegret@gmail.com>
2019-03-12 14:31:57 +0100

    [#164566404] added the first draft of a seed, commented out because bdd not ready yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Property.destroy_all 
+# ActiveRecord::Base.connexion.reset_pk_sequence('properties')
+
+# 20.times do 
+#   p = Property.new(
+#     title: Faker::Address.full_address,
+#     price: ,
+#     surface: ,
+#     description: Faker::Company.bs,
+#     image_id: rand(1..8),
+#     agency_id: rand(1..5),
+#     floor: rand(1..9),
+#     rooms: rand(1..4),
+#     area_id: rand(1..3),
+#     available_date: Faker::Date.forward(rand(10..50)),
+#     address: Faker::Address.street_address
+#   )
+# end
