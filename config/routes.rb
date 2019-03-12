28eb13c9f73ee002473dafe1951c2e850bfd872f
fr3d100 <f.bonnand@gmail.com>
2019-03-12 17:19:47 +0100

    [#164536451] update route and general layouts for devise integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-	root 'static_homes#index'
   get 'static_homes/index'
   devise_for :agencies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
