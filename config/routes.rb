04b6d0d05bccfdac69bacf55d81ba30bbb92627f
fr3d100 <f.bonnand@gmail.com>
2019-03-12 16:48:37 +0100

    [#164536451] first modification of signup view for agency

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+	root 'static_homes#index'
+  get 'static_homes/index'
   devise_for :agencies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
