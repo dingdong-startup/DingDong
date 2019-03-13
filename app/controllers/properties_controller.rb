dadeb714d41f3606e6a9b9d1745f71b23b1e88af
cacdesign <pro.nicolasfernandez@gmail.com>
2019-03-13 12:11:35 +0100

    [#164539458] show agency set up into partials

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,9 @@ class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
   end
+
+  def create
+  	puts params
+  	@property = Property.find(params[:id])
+  end
 end
