1b2aa966effd4bf58d0bf8c75c7236a8ca72189e
fr3d100 <f.bonnand@gmail.com>
2019-03-13 13:32:29 +0100

    [#164541948] Add a create feature for properties includong Active Storage

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,46 @@
 class PropertiesController < ApplicationController
   def show
     @property = Property.find(params[:id])
+    puts "#"*50
+    puts params[:agency_id]
+    puts "#"*50
+
   end
+  
+  def new
+  	@agency = Agency.find(params[:agency_id])
+    @property = Property.new
+  end
+
+  def create
+  	@property = Property.new(property_params)
+    @property.images.attach(params[:property][:images])
+    @property.agency_id = params[:agency_id]
+    @property.agent_id = 1
+    @property.area_id = 1
+
+    puts "#"*50
+    puts params[:property][:images]
+    puts "#"*50
+
+
+    puts "#"*50
+   	puts @property.inspect
+		puts "#"*50
+
+    if @property.save
+      flash[:success] = "Votre bien a été créé"
+      redirect_to root_path
+    else
+    flash[:danger] = @property.errors.messages
+    redirect_to new_agency_property_path
+    end
+  end
+
+  private
+
+  def property_params
+  	params.require(:property).permit(:title, :price, :surface, :description, :floor, :room, :available_date, :address)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   get 'agencies/show'
   get 'static_homes/index'
   devise_for :agencies
-  resources :agencies
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :agencies do 
-    resources :properties, only: [:show, :edit, :update, :delete]
+    resources :properties, except: [:index]
   end
 
   resources :properties, only: [:show]
