55cc3f07d093cb72e0ea3e2894255c1ce208035e
Segrelove <mlesegret@gmail.com>
2019-03-13 18:48:16 +0100

    [#164544832] added a stripe button, a method to register stripe customer id, and added the button to view show property

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -6,7 +6,30 @@ class VisitsController < ApplicationController
   end
 
   def create
-    v = Visit.new(property_id: params[:property_id], tenant_id: current_tenant.id, status_id: 1)
+    @visit = Visit.new(property_id: params[:property_id], tenant_id: current_tenant.id, status_id: 1)
+    @visit.save 
+
+    customer = Stripe::Customer.create({
+    source: 'tok_mastercard',
+    email: 'paying.user@example.com',
+    })
+
+    if customer.save 
+      current_tenant.update(stripe_customer_id: customer.id)
+      flash[:success] = "Votre visite est bien enregistrée !"
+      puts '*'*50
+      puts customer.id
+      puts '*'*50
+      redirect_to root_path
+    else       
+      puts '*'*50
+      puts customer.id
+      puts '*'*50
+    end
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_property_visit_path
   end
 
   def show
