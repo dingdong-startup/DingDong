35526ea850929789ef747412ee1b1dd9ef102af9
Segrelove <mlesegret@gmail.com>
2019-03-13 17:42:21 +0100

    [#164544832] added has_many associations, tenants has many visits

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,4 +3,5 @@ class Tenant < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :visits
 end
