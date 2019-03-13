ca30e1a7a99bbd9982541099f7873dddc841bd19
Segrelove <mlesegret@gmail.com>
2019-03-13 11:05:46 +0100

    [#164541549] updated the routes for agencies properties show method

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :agencies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'statics#index'
+  resources :agencies do 
+    resources :properties, only: [:show, :edit, :update, :delete]
+  end
+
+  root 'statics#index'
 
 end
