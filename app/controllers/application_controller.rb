85b2a15730d7d082f001745cd4c7ec58be66f94c
fr3d100 <f.bonnand@gmail.com>
2019-03-14 12:54:35 +0100

    [#164627846] in case of access denied, the user is redirected to root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
 
+rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      #format.json { head :forbidden }
+      format.html { redirect_to root_path, :alert => exception.message }
+    end
+  end
+
 end
