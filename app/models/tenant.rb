aaf463dd11a21a292537b2e1167ae7eb4522fadd
Segrelove <mlesegret@gmail.com>
2019-03-14 10:59:34 +0100

    [#164544832] added visit, visit status and payment status tables, linked them and tried them, it works just fine

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -3,4 +3,6 @@ class Tenant < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :visits
+  belongs_to :payment_status
 end
