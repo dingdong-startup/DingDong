b713214c721e62ccff0643d13df3896b393c9581
fr3d100 <f.bonnand@gmail.com>
2019-03-14 14:30:06 +0100

    [#164627846] display error message after re-rooting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
 
 rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      #format.json { head :forbidden }
-      format.html { redirect_to root_path, :alert => exception.message }
+      format.html { 
+      	flash[:danger] = exception.message
+      	redirect_to root_path
+      }
     end
   end
 
