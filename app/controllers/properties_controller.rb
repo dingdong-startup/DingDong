b85a3b2d1127cdbf4fcab8dda58ce42f47548df3
estebanpetaflop <etienne.chev@gmail.com>
2019-03-15 16:36:18 +0100

    [#164541750] controller and views updated for edit and delete of properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,15 +5,16 @@ class PropertiesController < ApplicationController
 
   def show
     @property = Property.find(params[:id])
+    @agency = Agency.find(params[:agency_id])
   end
 
   def new
-  	@agency = Agency.find(params[:agency_id])
+    @agency = Agency.find(params[:agency_id])
     @property = Property.new
   end
 
   def create
-  	@property = Property.new(property_params)
+    @property = Property.new(property_params)
     @property.images.attach(params[:property][:images])
     @property.agency_id = params[:agency_id]
     @property.agent_id = 1
@@ -23,25 +24,52 @@ class PropertiesController < ApplicationController
       flash[:success] = "Votre bien a été créé"
       redirect_to root_path
     else
-    flash[:danger] = @property.errors.messages
-    redirect_to new_agency_property_path
+      flash[:danger] = @property.errors.messages
+      redirect_to new_agency_property_path
     end
   end
 
   def edit
-end
+    @property = Property.find(params[:id])
+    @agency = Agency.find(params[:agency_id])
+  end
 
-def update
-end
+  def update
+    @property = Property.find(params[:id])
+    @agency = Agency.find(params[:agency_id])
+    puts "*"*60
+puts params
+puts params["/agencies/#{@agency.id}/properties/#{@property.id}"]
+    if @property.update(agency_property_params)
+      flash[:success] = "Votre bien a été mis à jour"
+      redirect_to agency_property_path(@agency,@property)
+    else
+      flash[:danger] = @property.errors.messages
+      redirect_to agency_property_path(@agency,@property)
+    end
+  end
 
-def destroy
-end
+  def destroy
+    @property = Property.find(params[:id])
+    @agency = Agency.find(params[:agency_id])
+    if @property.delete
+      flash[:success] = "Votre propriété a bien été supprimée"
+      redirect_to root_path
+    else
+      flash[:danger] = @property.errors.messages
+      redirect_to new_agency_property_path
+    end
+  end
 
 
   private
+  def agency_property_params
+    params.require("/agencies/#{@agency.id}/properties/#{@property.id}").permit(:title, :price, :surface, :description, :floor, :room, :available_date, :address)
+
+  end
 
   def property_params
-  	params.require(:property).permit(:title, :price, :surface, :description, :floor, :room, :available_date, :address)
+    params.require(:property).permit(:title, :price, :surface, :description, :floor, :room, :available_date, :address)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :agencies do 
+  resources :agencies do
     resources :properties, except: [:index]
   end
 
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   resources :tenants, only: [:show, :edit, :update]
 
   resources :charges, only: [:index, :create]
+
 
   root 'statics#index'
 
