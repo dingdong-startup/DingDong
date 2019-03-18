8afa81e32e40c493523f16b322eba71137344262
fr3d100 <f.bonnand@gmail.com>
2019-03-18 20:06:51 +0100

    [#164699673] creation of an after_create method to automaticaly create 2 empty agency employees (director & agent) after agency creation

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -2,10 +2,31 @@ class Agency < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
   has_many :agency_groups
   has_many :groups, through: :agency_groups
   
   has_many :agents
   has_many :properties
+
+  after_create :create_agents
+
+
+  def create_agents
+
+  	director = Agent.create(first_name: "",
+  		last_name: "",
+  		email: "",
+  		cellphone: "",
+  		position_id: 1,
+  		agency_id: self.id)
+
+  	agent = Agent.create(first_name: "",
+  		last_name: "",
+  		email: "",
+  		cellphone: "",
+  		position_id: 2,
+  		agency_id: self.id)
+
+  end
 end
