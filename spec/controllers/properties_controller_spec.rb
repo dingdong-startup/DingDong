b1ace38b8424bacba066ebd45b4d7ea10e2bab1c
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-03-21 15:55:48 +0100

    [#164532024] add property test

--- spec/controllers/properties_controller_spec.rb
+++ spec/controllers/properties_controller_spec.rb
@@ -16,28 +16,4 @@ RSpec.describe PropertiesController, type: :controller do
       
       end
 
-  describe "GET #show" do
-    # des tests
-  end
-
-  describe "GET #new" do
-    # des tests
-  end
-
-  describe "GET #edit" do
-    # des tests
-  end
-
-  describe "POST #create" do
-    # des tests
-  end
-
-  describe "PUT #update" do
-    # des tests
-  end
-
-  describe "DELETE #destroy" do
-    # des tests
-  end
-
 end
