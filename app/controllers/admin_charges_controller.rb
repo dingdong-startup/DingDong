3915fb80af8e10d3a5956867ddb110eddb26aa33
fr3d100 <f.bonnand@gmail.com>
2019-03-22 15:57:11 +0100

    [#164826446] cleaning AdminCharges controller

--- app/controllers/admin_charges_controller.rb
+++ app/controllers/admin_charges_controller.rb
@@ -1,21 +1,17 @@
 class AdminChargesController < ApplicationController
   def index
     @tenants = Tenant.where(payment_status_id: 2)
-    puts @tenants
   end
 
   def create
     stripe_customer_id = params[:stripe_customer_id]
-    puts stripe_customer_id
-    puts "*"*60
-    puts "Je suis en passe de marcher"
     tenant = Tenant.last
     charge = Stripe::Charge.create({
       amount: 9900,
       currency: 'eur',
       customer: stripe_customer_id,
     })
-    puts "c'est BON"
+
     if charge.save 
       flash[:success] = "L'utilisateur a bien été débitée !"
       redirect_to root_path
@@ -28,4 +24,5 @@ class AdminChargesController < ApplicationController
     flash[:error] = e.message
     redirect_to charge_index_path
   end
+
 end
