3e89cdd6c317d3c58da95d2adab9c479c3f7d2c6
fr3d100 <f.bonnand@gmail.com>
2019-03-22 17:59:02 +0100

    [#164826446] add email after visit requested

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,7 +1,14 @@
 class Visit < ApplicationRecord
+
+	after_save :new_visit_email_send
+
   belongs_to :tenant
   belongs_to :property
   belongs_to :visit_status   
+
+ 	def new_visit_email_send
+  	TenantMailer.new_visit_email(self.tenant, self.property).deliver_now
+  end
 
 
 end
