3ea010ff69adee28d17a7fd374fe833c11d1ba70
fr3d100 <f.bonnand@gmail.com>
2019-03-22 07:34:09 +0100

    [#164803285] Edit agencies routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,20 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :tenants
-  get 'agencies/index'
-  get 'agencies/show'
+
   get 'static_homes/index'
 
-  devise_for :agencies
+  devise_for :agencies, except: [:index]
   authenticated :agency do
     root :to => 'agencies#show'
   end
-
+  # resources :agencies, except: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :agencies do 
+ 
+
+  resources :agencies, except: [:index]  do 
     resources :properties, except: [:index]
   end
 
