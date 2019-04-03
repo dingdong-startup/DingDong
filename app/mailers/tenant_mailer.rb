bb6c34f2bad99d2610881693c3385c7e9a973d5b
estebanpetaflop <etienne.chev@gmail.com>
2019-04-03 13:29:13 +0200

    [#165052778] mail object updated

--- app/mailers/tenant_mailer.rb
+++ app/mailers/tenant_mailer.rb
@@ -3,7 +3,7 @@ class TenantMailer < ApplicationMailer
   def new_visit_email(tenant, property)
   	@tenant = tenant
   	@property = property
-  	mail(to: @tenant.email, subject: "Ding Dong! Ta demande de visite a bien été enregistrée")
+  	mail(to: @tenant.email, subject: "Ding Dong! Votre demande de visite a bien été enregistrée")
   end
 
   def payment_email(tenant, property)
