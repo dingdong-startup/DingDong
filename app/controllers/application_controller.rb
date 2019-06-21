1ae658b6923e76d79237b9cf6ef784f76cc69f7e
Segrelove <mlesegret@gmail.com>
2019-06-21 14:49:29 +0200

    added a method to redirect back to same page with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	before_action :store_user_location!, if: :storable_location?
+	after_action :store_action
 
 	protected
 
@@ -10,13 +11,13 @@ class ApplicationController < ActionController::Base
 	end
 
 	def storable_location?
-      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
-    end
+		request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+	end
 
-    def store_user_location!
+	def store_user_location!
       # :user is the scope we are authenticating
-      store_location_for(:user, request.fullpath)
-    end
+		store_location_for(:user, request.fullpath)
+	end
 
 	def after_sign_in_path_for(resource)
 		if current_agency
@@ -24,6 +25,24 @@ class ApplicationController < ActionController::Base
   	else
   		super
   	end
-	end
+	end	
+
+	# def after_sign_in_path_for(resource)
+	# 	properties_path
+	# end
+
+  
+  def store_action
+    return unless request.get? 
+    if (request.path != "/tenants/sign_in" &&
+        request.path != "/tenants/sign_up" &&
+        request.path != "/tenants/password/new" &&
+        request.path != "/tenants/password/edit" &&
+        request.path != "/tenants/confirmation" &&
+        request.path != "/tenants/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:tenant, request.fullpath)
+    end
+  end
 
 end
